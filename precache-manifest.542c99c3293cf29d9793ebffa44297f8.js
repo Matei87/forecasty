62206a9ae3d262882494e22e3f21dd712b4a0325
Matei Mircea <mrc_matei@yahoo.com>
2020-09-29 15:56:41 +0300

    Updates

---
+++ precache-manifest.542c99c3293cf29d9793ebffa44297f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26498f1e0490e2296f02b53cfc222117",
+    "url": "/forecasty/index.html"
+  },
+  {
+    "revision": "ca2e08dc4501e88506a1",
+    "url": "/forecasty/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c31eaefe6edde3074967",
+    "url": "/forecasty/static/css/main.50ad9b5e.chunk.css"
+  },
+  {
+    "revision": "ca2e08dc4501e88506a1",
+    "url": "/forecasty/static/js/2.15d125ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/forecasty/static/js/2.15d125ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c31eaefe6edde3074967",
+    "url": "/forecasty/static/js/main.37a37faa.chunk.js"
+  },
+  {
+    "revision": "897a3ada27354fed636b",
+    "url": "/forecasty/static/js/runtime-main.c6185068.js"
+  }
+]);
